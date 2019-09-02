29fab621ee907a84c2110969372d218b0024a5bb
dannypc1628 <h152074716@gmail.com>
2019-09-02 10:15:51 +0800

    加入專案檔案。

---
+++ ChatRoom/Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("收到訊息", user,message);
+        }
+        public async Task Login(string user)
+        {
+            await Clients.All.SendAsync("進入大廳事件", "系統通知", user+" 進入聊天室大廳");
+        }
+
+
+
+
+        public Task SendPrivateMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+    }
+}
---
+++ ChatRoom/wwwroot/js/chat.js
@@ -0,0 +1,110 @@
+﻿//"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+var app = new Vue({
+    el: '#chatPage',
+    data: {
+        name: "",
+        message: "",
+        groupName:"",
+        status:0,
+        messageList: [],
+        groupMessageList:[]
+    },
+    methods: {
+        login: function () {
+            console.log("按下login");
+            
+            connection
+                .invoke("Login", this.name)
+                .then(() => {
+                    this.status = 1
+                    //接收訊息
+                    connection.on("收到訊息", (user, message) => {
+                        if (app.status == 1)
+                            pushMessageList(user, message);
+                    });
+                    connection.on("進入大廳事件", (user, message) => pushMessageList(user, message));
+                })
+                .catch(function (err) {
+                    return console.error(err.toString());
+                });
+
+        },
+        send: function () {
+            
+            console.log("按下送出訊息");
+            connection
+                .invoke("SendMessage", this.name, this.message)
+                .then(() => {
+                    this.message = "";
+                })
+                .catch(function (err) {
+                    return console.error(err.toString());
+                });
+            
+        },
+        goRoom: function () {
+            console.log("按下進入聊天室");
+            connection
+                .invoke("AddToGroup", this.groupName)
+                .then(() => {
+                    app.messageList.length = 0;
+                })
+                .catch(function (err) {
+                    return console.error(err.toString());
+                });
+        }
+        
+    }
+
+    
+});
+
+
+//Disable send button until connection is established
+//document.getElementById("sendButton").disabled = true;
+
+connection.start()
+    .then(function () {
+        console.log("連線到伺服器");
+        //document.getElementById("sendButton").disabled = false;
+    })
+    .catch(function (err) {
+        return console.error(err.toString());
+    })
+
+
+
+
+function setList (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " 說 " + msg;
+    ;
+    var li = document.createElement("li");
+
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+}
+function pushMessageList(user, message) {
+    console.log("觸發pushMessageList")
+    app.messageList.push({
+        'user': user,
+        'message': message
+    })
+}
+
+
+//送出訊息
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    console.log("按下送出訊息");
+    connection
+        .invoke("SendMessage", user, message)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+});
+
